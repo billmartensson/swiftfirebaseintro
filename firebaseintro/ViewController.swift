004f871b252c7321e535c3e14d5c8d767cd75197
Bill Mårtensson <bill@magictechnology.se>
2020-09-16 15:19:43 +0200

    Initial Commit

---
+++ firebaseintro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebaseintro
+//
+//  Created by Bill Martensson on 2020-09-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
