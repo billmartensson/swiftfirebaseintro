84c2b969449ad02dc3f27279e44bfcd1799fad25
Bill Mårtensson <bill@magictechnology.se>
2020-09-16 16:12:46 +0200

    Start firebase

---
+++ Pods/Target Support Files/Pods-firebaseintro/Pods-firebaseintro-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_firebaseintro : NSObject
+@end
+@implementation PodsDummy_Pods_firebaseintro
+@end
--- firebaseintro/ViewController.swift
+++ firebaseintro/ViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class ViewController: UIViewController {
+
+    var ref: DatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        ref = Database.database().reference()
+        
+        self.ref.child("theuser").setValue(["username": "torsten"])
+        
+        ref.child("firstname").observeSingleEvent(of: .value, with: { (snapshot) in
+          
+            var thefirstname = snapshot.value as! String
+            
+            print(thefirstname)
+            
+        }) { (error) in
+            print(error.localizedDescription)
+        }
+        
+        
     }
 
 
